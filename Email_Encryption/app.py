9b8b8290a4ba8edd005e83296cfe5ab7aebcfc2e
Toqa Hassib <70861082+Toqahassib@users.noreply.github.com>
2022-12-23 00:51:38 +0200

    Application Front End

--- Email_Encryption/app.py
+++ Email_Encryption/app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/sendmail', methods=('GET', 'POST'))
+def sendmail():
+    return render_template("mail.html")
+
+
+@app.route('/decrypt', methods=('GET', 'POST'))
+def decrypt():
+    return render_template('decrypt.html')
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
---
+++ Email_Encryption/static/js/main.js
@@ -0,0 +1,24 @@
+// Theme
+
+const themeToggle = document.querySelector(".checkbox");
+const body = document.querySelector("body");
+
+// dark mode will stay after refreshing the page
+const darkmode = localStorage.getItem("dark");
+
+if (darkmode) {
+  body.classList.add("dark");
+  themeToggle.checked = true;
+}
+
+themeToggle.addEventListener("change", function () {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+
+    // dark mode will stay after refreshing the page
+    localStorage.setItem("dark", "active");
+  } else {
+    localStorage.removeItem("dark");
+  }
+});
